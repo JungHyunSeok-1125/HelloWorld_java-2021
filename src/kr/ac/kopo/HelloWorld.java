105afe6e4da978cb2b060e1d3a19459e9a310e77
Jung Hyun Seok <belbel0845@gmail.com>
2021-03-04 11:39:28 +0900

    [Add]First commit

---
+++ src/kr/ac/kopo/HelloWorld.java
@@ -0,0 +1,15 @@
+package kr.ac.kopo;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is Hello World");
+		System.out.println("Hello There");
+		System.out.println("Hi World");
+	
+		
+	}
+
+}
